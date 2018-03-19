a682d8d3fe988264617d7361ddfc1b2e5b4cd3bc
shock <github@wdoughty.net>
2018-03-19 15:02:45 -0500

    Added edit, update, and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,17 +5,44 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      puts @article.inspect
+      puts @article.errors.any?
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
 private
