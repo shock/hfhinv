2513672e0e727e9beb74c66ca020327a21b5c0ee
shock <github@wdoughty.net>
2018-03-22 14:28:43 -0500

    AddDescriptionToItem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322192624) do
+ActiveRecord::Schema.define(version: 20180322192810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20180322192624) do
     t.string "rejection_reason"
     t.integer "donation_id"
     t.string "inventory_number"
+    t.string "description"
   end
 
   create_table "use_of_items", force: :cascade do |t|
