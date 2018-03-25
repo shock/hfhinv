52e9679ce3ff008a3fd3676ad3f0a0a555eb1b35
shock <github@wdoughty.net>
2018-03-25 11:00:33 -0500

    Include # of donations for Donors

--- app/admin/donors.rb
+++ app/admin/donors.rb
@@ -40,6 +40,7 @@ ActiveAdmin.register Donor do
     column :zip
     column :phone do |donor| format_phone_number(donor.phone) rescue nil end
     # column :phone2 do |donor| format_phone_number(donor.phone2) rescue nil end
+    column :num_donations, label: '# donations' do |donor| donor.donations.count; end
     actions name: 'Actions'
   end
 
@@ -51,6 +52,7 @@ ActiveAdmin.register Donor do
       row :zip
       row :phone_numbers
       row :email
+      row :number_of_donations do |donor| donor.donations.count; end
     end
 
     panel "Donations (Pickups)" do
