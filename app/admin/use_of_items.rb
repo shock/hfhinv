46266b2bf4862b5629e2d665045fee13b0701d32
shock <github@wdoughty.net>
2018-03-26 12:37:00 -0500

    Fix AA UseOfItems

--- app/admin/use_of_items.rb
+++ app/admin/use_of_items.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register UseOfItem do
   config.batch_actions = false if Rails.env.production?
   menu parent: 'Admin'
   config.sort_order = 'name_asc'
+  config.filters = false
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -20,12 +21,14 @@ ActiveAdmin.register UseOfItem do
     selectable_column
     id_column unless Rails.env.production?
     column :name
-    actions name: 'Actions', defaults: false do |item|
+    actions name: 'Actions', defaults: false do |use_of_item|
       output = []
-      output << link_to("View", admin_item_path(item))
-      output << link_to("Edit", edit_admin_item_path(item))
-      output << link_to("Delete", admin_item_path(item), method: :delete,
-        data: {confirm: "Are you absolutely sure you want to delete #{item.name}? If any items exist with this use type, you will corrupt the database!!"})
+      output << link_to("View", admin_use_of_item_path(use_of_item))
+      output << link_to("Edit", edit_admin_use_of_item_path(use_of_item))
+      unless use_of_item.items.count > 0
+        output << link_to("Delete", admin_use_of_item_path(use_of_item), method: :delete,
+          data: {confirm: "Are you sure you want to delete #{use_of_item.name}?"})
+      end
       output.join(' ').html_safe
     end
   end
